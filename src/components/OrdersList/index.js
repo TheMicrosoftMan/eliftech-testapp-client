145c2dd39b0a7d2adea933ba6cabdbbfab82a6b9
TheMircosoftMan <yurazelenko@windowslive.com>
2020-02-12 16:14:53 +0200

    Add possibility to sort rows on the table by clicking on head column

--- src/components/OrdersList/index.js
+++ src/components/OrdersList/index.js
@@ -1,63 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 
 const SORT_TYPES = {
-  UP: {
-    type: "UP",
-    fn: (a, b) => {
-      return a.value - b.value;
-    }
-  },
-  DOWN: {
-    type: "DOWN",
-    fn: (a, b) => {
-      return b.value - a.value;
-    }
-  },
-  DEFAULT: {
-    type: "DEFAULT",
-    fn: (a, b) => a
-  }
+  ASC: "ASC",
+  DESC: "DESC"
 };
 
 const OrdersList = ({ orders }) => {
-  const [valueSorting, setValueSorting] = useState(SORT_TYPES.DEFAULT.type);
+  const [ordersList, setOrdersList] = useState([]);
+  const [currentSortType, setCurrentSortType] = useState(SORT_TYPES.ASC);
 
-  const sortChange = () => {
-    let nextSort;
+  useEffect(() => {
+    setOrdersList(orders);
+  }, [orders]);
 
-    if (valueSorting === SORT_TYPES.DOWN.type) nextSort = SORT_TYPES.UP.type;
-    else if (valueSorting === SORT_TYPES.UP.type)
-      nextSort = SORT_TYPES.DEFAULT.type;
-    else if (valueSorting === SORT_TYPES.DEFAULT.type)
-      nextSort = SORT_TYPES.DOWN.type;
+  const compare = key => {
+    switch (currentSortType) {
+      case SORT_TYPES.ASC:
+        return (a, b) => {
+          if (a[key] < b[key]) return 1;
+          if (a[key] > b[key]) return -1;
+          return 0;
+        };
 
-    setValueSorting(nextSort);
+      case SORT_TYPES.DESC:
+        return (a, b) => {
+          if (a[key] < b[key]) return -1;
+          if (a[key] > b[key]) return 1;
+          return 0;
+        };
+
+      default:
+        return (a, b) => {
+          return 0;
+        };
+    }
+  };
+
+  const nextSortType = () => {
+    switch (currentSortType) {
+      case SORT_TYPES.ASC:
+        setCurrentSortType(SORT_TYPES.DESC);
+        break;
+
+      case SORT_TYPES.DESC:
+        setCurrentSortType(SORT_TYPES.ASC);
+        break;
+
+      default:
+        setCurrentSortType(SORT_TYPES.ASC);
+        break;
+    }
+  };
+
+  const sortBy = key => {
+    let arrayCopy = ordersList;
+    arrayCopy.sort(compare(key));
+    setOrdersList([...arrayCopy]);
+    nextSortType();
   };
 
   return (
     <div className="OrdersList">
-      {orders.length > 0 ? (
+      {ordersList.length > 0 ? (
         <table className="OrdersList__table">
           <thead>
             <tr>
-              <th>User email</th>
-              <th>Date</th>
-              <th onClick={sortChange}>Value {valueSorting}</th>
-              <th>Currency</th>
-              <th>Status</th>
+              <th onClick={() => sortBy("user_email")}>User email</th>
+              <th onClick={() => sortBy("date")}>Date</th>
+              <th onClick={() => sortBy("value")}>Value</th>
+              <th onClick={() => sortBy("currency")}>Currency</th>
+              <th onClick={() => sortBy("status")}>Status</th>
             </tr>
           </thead>
           <tbody>
-            {[...orders].sort(SORT_TYPES[valueSorting].fn).map(order => (
-              <tr key={order._id}>
-                <td>{order.user_email}</td>
-                <td>{moment(order.date).format("DD.MM.YYYY HH:mm:ss")}</td>
-                <td>{order.value}</td>
-                <td>{order.currency}</td>
-                <td>{order.status}</td>
-              </tr>
-            ))}
+            {ordersList.map(order => {
+              return (
+                <tr key={order._id}>
+                  <td>{order.user_email}</td>
+                  <td>{moment(order.date).format("DD.MM.YYYY HH:mm:ss")}</td>
+                  <td>{order.value}</td>
+                  <td>{order.currency}</td>
+                  <td>{order.status}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       ) : (
